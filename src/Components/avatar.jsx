8634f1588e3b952c0c5d58ac0691df8162bab1c7
CHANDNI JHA <96429544+ChandniJha630@users.noreply.github.com>
2024-12-08 15:39:26 +0530

    Update avatar.jsx

--- src/Components/avatar.jsx
+++ src/Components/avatar.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import DP from "../assets/profile.jpg"
+import Profile from "../assets/profile.jpg"
 import Stats from './stats'
 import '../styles/avatar.css'
 
@@ -8,7 +8,7 @@ function Avatar() {
     <div className='flex profile'>
     <div className="avatar">
     <div className="w-64 lg:w-32 border-4 lg:border border-yellow-400 rounded-full">
-    <img src={DP} />
+    <img src={Profile} />
     </div>
 </div>
 <div><Stats/></div>
